7cb695638b3ca485b7a60e8e1dc6f4f0ed42e998
JordanHenman <122462748+JordanHenman@users.noreply.github.com>
2024-11-20 09:35:35 -0500

    Update decryptor.sh

--- decryptor.sh
+++ decryptor.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Defines image directories.
-enc_image_dir="/pathToEncryptedImages"
-dec_image_dir="/pathToDecryptedImages"
+enc_image_dir="/pathToEncryptedImages/enc_image_dir"
+dec_image_dir="/pathToDecryptedImages/dec_image_dir"
 
 # Creates output_dir if it doesn't exist. 
 if [ ! -d "$dec_image_dir" ]; then       
