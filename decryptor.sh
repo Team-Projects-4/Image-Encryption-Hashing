6b2bfaf65ca92c7f7ce694c4ee38f6fd97b303eb
JordanHenman <122462748+JordanHenman@users.noreply.github.com>
2024-11-20 09:31:52 -0500

    Create decryptor.sh

---
+++ decryptor.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Defines image directories.
+enc_image_dir="/pathToEncryptedImages"
+dec_image_dir="/pathToDecryptedImages"
+
+# Creates output_dir if it doesn't exist. 
+if [ ! -d "$dec_image_dir" ]; then       
+        mkdir -p "$dec_image_dir"
+fi 
+
+# Decrypts every image inside of enc_image_dir.
+for image in "$enc_image_dir"/*; do
+        if [ -f "$image" ]; then
+        # Encrypts every image with openssl aes and saves the output to output_dir
+                openssl enc -aes-256-cbc -pass pass:group4 -d -in "$image" -out "$dec_image_dir/$(basename "$image").png" -P
+        fi
+done 
