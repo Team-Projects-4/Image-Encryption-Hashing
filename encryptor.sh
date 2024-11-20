d4bf6a6642905a619ad53aa1d3caca3d4476f6b1
JordanHenman <122462748+JordanHenman@users.noreply.github.com>
2024-11-20 09:17:30 -0500

    Update encryptor.sh

--- encryptor.sh
+++ encryptor.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-image_dir= /pathToDeathStarImages
-# Specifies where Death Star images are. 
 
-output_dir= /pathToEncrytpedImages
-# Specifies where to save encrypted images. 
+# Defines image directories.
+image_dir="/pathToDeathStarImages" # Specifies where Death Star images are. 
+output_dir="/pathToEncrytpedImages" # Specifies where to save encrypted images. 
 
-if [ ! -d "$output_dir" ]; then       
-        mkdir "$output_dir"
-fi
 # Creates output_dir if it doesn't exist. 
+if [ ! -d "$output_dir" ]; then       
+        mkdir -p "$output_dir"
+fi 
 
-for image in "$image_dir"/; do
-        if [ -f "$image" ]; then
-                openssl enc -aes-256-cbc -pass pass:group4 -p -in "$image" -out "$image".enc
-        fi
-done
 # Encrypts every image inside of the image_dir directory. 
+for image in "$image_dir"/*; do
+        if [ -f "$image" ]; then
+        # Encrypts every image with openssl aes and saves the output to output_dir
+                openssl enc -aes-256-cbc -pass pass:group4 -p -in "$image" -out "$output_dir/$(basename "$image").enc"
+        fi
+done 
