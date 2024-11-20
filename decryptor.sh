741251e6eb46c4c3059be0e83fb5c78b6937694d
JordanHenman <122462748+JordanHenman@users.noreply.github.com>
2024-11-20 09:36:15 -0500

    Update decryptor.sh

--- decryptor.sh
+++ decryptor.sh
@@ -12,7 +12,7 @@ fi
 # Decrypts every image inside of enc_image_dir.
 for image in "$enc_image_dir"/*; do
         if [ -f "$image" ]; then
-        # Encrypts every image with openssl aes and saves the output to output_dir
+        # Decrypts every image and saves the output to output_dir
                 openssl enc -aes-256-cbc -pass pass:group4 -d -in "$image" -out "$dec_image_dir/$(basename "$image").png" -P
         fi
 done 
