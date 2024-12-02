c4d84d4667fae920e5ec300775383896f97d6b71
JordanHenman <122462748+JordanHenman@users.noreply.github.com>
2024-12-02 09:27:18 -0500

    Update decryptor.sh

--- decryptor.sh
+++ decryptor.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
 # Defines image directories.
-enc_image_dir="/pathToEncryptedImages/enc_image_dir"
-dec_image_dir="/pathToDecryptedImages/dec_image_dir"
+enc_image_dir="enc_image_dir/"
+dec_image_dir="dec_image_dir/"
 
-# Creates dec_image_dir if it doesn't exist. 
-if [ ! -d "$dec_image_dir" ]; then       
+# Creates dec_image_dir if it doesn't exist.
+if [ ! -d "$dec_image_dir" ]; then
         mkdir -p "$dec_image_dir"
-fi 
+fi
 
 # Decrypts every image inside of enc_image_dir.
 for image in "$enc_image_dir"/*; do
         if [ -f "$image" ]; then
+
+                filename=$(basename "$image")
+                new_filename="${filename%.*}"
+                decrypted_image="dec_image_dir/dec_$new_filename.png"
         # Decrypts every image and saves the output to dec_image_dir
-                openssl enc -aes-256-cbc -pass pass:group4 -d -in "$image" -out "$dec_image_dir/dec_$(basename "$image").png" -P
+                openssl enc -aes-256-cbc -pass pass:group4 -d -in "$image" -out "$decrypted_image" -P
         fi
-done 
+done
