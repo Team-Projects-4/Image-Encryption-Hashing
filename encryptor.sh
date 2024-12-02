12d1db6f9b80c003c3dfaaf6acd13afa90a7d3c4
JordanHenman <122462748+JordanHenman@users.noreply.github.com>
2024-12-02 08:49:09 -0500

    Update encryptor.sh

--- encryptor.sh
+++ encryptor.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
 # Defines image directories.
-image_dir="/pathToDeathStarImages/image_dir" # Specifies where Death Star images are. 
-enc_image_dir="/pathToEncrytpedImages/enc_image_dir" # Specifies where to save encrypted images. 
+image_dir="image_dir/" # Specifies where Death Star images are.
+enc_image_dir="enc_image_dir/" # Specifies where to save encrypted images.
 
-# Creates enc_image_dir if it doesn't exist. 
-if [ ! -d "$enc_image_dir" ]; then       
-        mkdir -p "$enc_imgae_dir"
-fi 
+# Creates enc_image_dir if it doesn't exist.
+if [ ! -d "$enc_image_dir" ]; then
+        mkdir -p "$enc_image_dir"
+fi
 
-# Encrypts every image inside of the image_dir directory. 
+# Encrypts every image inside of the image_dir directory.
 for image in "$image_dir"/*; do
         if [ -f "$image" ]; then
+
+                filename=$(basename "$image")
+                encrypted_image="enc_image_dir/$filename.enc"
         # Encrypts every image with openssl aes and saves the output to enc_image_dir
-                openssl enc -aes-256-cbc -pass pass:group4 -p -in "$image" -out "$enc_image_dir/$(basename "$image").enc"
+                openssl enc -aes-256-cbc -pass pass:group4 -p -in "$image" -out "$encrypted_image"
         fi
-done 
+done
