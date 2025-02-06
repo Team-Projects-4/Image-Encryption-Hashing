a902a51e2a5813cc9548f61812280cb71b0eb673
JordanHenman <122462748+JordanHenman@users.noreply.github.com>
2025-02-06 09:08:54 -0500

    Update hashDecrypt.sh

--- hashDecrypt.sh
+++ hashDecrypt.sh
@@ -3,22 +3,18 @@
 # This is the primary script used to verify the hash and decrypt the encrypted images.
 # Requires chmod 770
 
-# Defines images directories.
+# Defines image and hash directories.
 transmission_dir="transmission_dir/"
 dec_image_dir="dec_image_dir/"
-transmission_hash_dir="transsmission_hash_dir/"
+thash_dir="thash_dir/"
+rhash_dir="rhash_dir/"
 
 # Sets retransmit to true.
-retransmit=True
+retransmit=true
 
 # Creates the transmission_dir if it doesn't exist.
 if [ ! -d "$transmission_dir" ]; then
         mkdir -p "$transmission_dir"
-fi
-
-# Creates the enc_image_dir if it doesn't exist.
-if [ ! -d "$enc_image_dir" ]; then
-        mkdir -p "$enc_image_dir"
 fi
 
 # Creates the dec_image_dir if it doesn't exist.
@@ -26,9 +22,14 @@ if [ ! -d "$dec_image_dir" ]; then
         mkdir -p "$dec_image_dir"
 fi
 
-# Creates the transmission_hash_dir if it doesn't exist.
-if [ ! -d "$transmission_hash_dir" ]; then
-        mkdir -p "$transmission_hash_dir"
+# Creates the thash_dir if it doesn't exist.
+if [ ! -d "$thash_dir" ]; then
+        mkdir -p "$thash_dir"
+fi
+
+# Creates the rhash_dir if it doesn't exist.
+if [ ! -d "$rhash_dir" ]; then
+        mkdir -p "$rhash_dir"
 fi
 
 for transmission in "$transmission_dir"/*; do
@@ -37,19 +38,21 @@ for transmission in "$transmission_dir"/*; do
                 # Strip the hash from the transmission.
                 hashname1=$(basename "$transmission")
                 tHash="${hashname1%.*}"
-                tail -c 32 $transmission >> transmission_hash_dir/$tHash.txt
+                tail -c 32 $transmission >> thash_dir/$tHash.txt
 
                 # Strip the hash from the transmission.
-                truncate -s -32 $transmisson
+                truncate -s -32 $transmisson.enc
                 
                 # Hash the transmission again.
                 hashname2=$(basename "$transmission")
                 rHash="${hashname2%.*}"
-                md5sum $transmission > transmission_hash_dir/$rHash.txt
+                md5sum $transmission > rhash_dir/$rHash.txt
+
+                # Removes extra non-necessary information from the hash file.
+                truncate -s 32 rhash_dir/$rhash.txt
 
                 # Compare the hashes to verify they are the same.
-
-                if [ "$tHash" == "$rHash" ]; then
+                if [ "$tHash" = "$rHash" ]; then
 
                         # If the hashes match retransmit = false.
                         retransmit=false
