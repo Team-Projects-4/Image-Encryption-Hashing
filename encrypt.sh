583e75e54766daa6804c21e17376c83c695e2b9f
Liam Tolkkinen <liamtolkki@gmail.com>
2025-04-05 13:05:49 -0400

    fixed path

--- encrypt.sh
+++ encrypt.sh
@@ -1,7 +1,7 @@
 #/!bin/sh
 
 image_dir="image_dir/" # Specifies where Death Star images are.
-enc_image_dir="/home/$USER/repos/TX-Image/imgTX" # Specifies where to save encrypted images.
+enc_image_dir="/home/$USER/repos/TX-Image/imgTX/" # Specifies where to save encrypted images.
 
 if [ ! -d "$enc_image_dir" ]; then
         mkdir -p "$enc_image_dir"
@@ -15,6 +15,6 @@ for image in "$image_dir"/*; do
                 new_filename="${filename%.*}"
                 enc_image="$new_filename.enc"
                 # Encrypts every image with openssl aes and saves the output to enc_image_dir
-                openssl enc -aes-256-cbc -k group4 -p -in "$image" -out "enc_image_dir/$enc_image"
+                openssl enc -aes-256-cbc -k group4 -p -in "$image" -out "$enc_image_dir$enc_image"
           fi
 done
